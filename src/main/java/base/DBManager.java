8886e72eea1aa5d7f96c5df47d4a8451709a5ff3
hahahanabi <forbussinesfighter@gmail.com>
2025-07-16 17:35:09 +0900

    AWSのRDS(MySQL)接続のための記述

--- src/main/java/base/DBManager.java
+++ src/main/java/base/DBManager.java
@@ -14,16 +14,28 @@ public class DBManager {
     	Connection con = null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/stock_management","root","root");
+            //con = DriverManager.getConnection("jdbc:mysql://localhost:8889/stock_management","root","root");
+            String env = System.getenv("APP_ENV"); // 環境切り替え変数
+
+            String url;
+            String user;
+            String pass;
+
+            if ("production".equals(env)) {
+                // Render（railway）用：環境変数から取得
+                url = System.getenv("jdbc:mysql://myapp-db.c6hs8mccoy1b.us-east-1.rds.amazonaws.com:3306/stock_management_pro?useSSL=true&requireSSL=true");
+                user = System.getenv("mydb_user");
+                pass = System.getenv("teipen0717");
+            } else {
+                // ローカル開発用
+                url = "jdbc:mysql://localhost:8889/stock_management";
+                user = "root";
+                pass = "root";
+            }
+
+            con = DriverManager.getConnection(url, user, pass);
             System.out.println("DBConnected!!");
             return con;
-//        }catch(ClassNotFoundException e){
-//            throw new IllegalMonitorStateException();
-//        }catch(SQLException e){
-//            throw new IllegalMonitorStateException();
-//        }catch(Exception e){
-//            throw new IllegalMonitorStateException();
-//        }
         } catch (ClassNotFoundException e) {
             e.printStackTrace(); // ここでログ出力
             throw new SQLException("JDBCドライバが見つかりません", e);
